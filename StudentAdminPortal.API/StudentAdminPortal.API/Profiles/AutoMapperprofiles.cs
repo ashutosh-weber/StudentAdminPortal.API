b8fc6c0fa630fdfc0ab36348a05967af0ca0ae86
ashutosh <ashu20043@gmail.com>
2022-07-05 15:24:59 +0530

    section 2 end of api

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperprofiles.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using StudentAdminPortal.API.DomainModels;
+using DataModels = StudentAdminPortal.API.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperprofiles: Profile
+    {
+        public AutoMapperprofiles()
+        {
+            CreateMap<DataModels.Student, Student>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Gender, Gender>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Address, Address>()
+                .ReverseMap();
+
+
+
+        }
+    }
+}
